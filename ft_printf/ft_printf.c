216d571a3bececbc4b41ee2800f34dc4e26c23c0
Miyuki <108064810+ito-miyuki@users.noreply.github.com>
2023-11-27 17:24:34 +0200

    add edge cases test for main function
    main function is commented out though

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mito <mito@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 13:45:33 by mito              #+#    #+#             */
-/*   Updated: 2023/11/26 13:10:50 by mito             ###   ########.fr       */
+/*   Updated: 2023/11/27 14:53:15 by mito             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,23 +36,57 @@ int	ft_printf(const char *str, ...)
 		str++;
 	}
 	va_end(ap);
-	if (count == -1)
-		return (-1);
 	return (count);
 }
+
 /*
-int	main()
+int main()
 {
+	//%%%
 	int	test;
 	int	test2;
-
-	test = ft_printf("\001\002\007\v\010\f\r\n");
-	printf("%c", '\n');
+	
+	test = ft_printf("%%%", "Miyuki");
 	ft_printf("How many chars: %d\n", test);
 	printf("%c", '\n');
-	test2 = printf("\001\002\007\v\010\f\r\n");
-	printf("%c", '\n');
+	test2 = printf("%%%", "Miyuki");
 	printf("How many chars: %d\n", test2);
+
+	//%space
+	int	test5;
+	int	test6;
+	test5 = ft_printf("%");
+	ft_printf("How many chars: %d\n", test5);
+	// printf("%c", '\n');
+	test6 = printf("%");
+	printf("How many chars: %d\n", test6);
+
+	//{hey%# y#}
+	int	test7;
+	int	test8;
+	test7 = ft_printf("{hey%# y#}", "Miyuki");
+	ft_printf("How many chars: %d\n", test7);
+	printf("%c", '\n');
+	test8 = printf("{hey%# y#}", "Miyuki");
+	printf("How many chars: %d\n", test8);
+
 	return (0);
+}
+
+*/
+/*
+int main(void)
+{
+    int	test3;
+	int	test4;
+	
+	//test3 = ft_printf("Hello, \n%s", "Miyuki");
+	test3 = ft_printf("%", "Miyuki");
+	ft_printf("How many chars: %d\n", test3);
+	printf("%c", '\n');
+	test4 = printf("Hello, \n%s", "Miyuki");
+	test4 = printf("%", "Miyuki");
+	printf("How many chars: %d\n", test4);
+    return (0);
 }
 */
--- ft_printf/print_ptr.c
+++ ft_printf/print_ptr.c
@@ -6,7 +6,7 @@
 /*   By: mito <mito@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 10:04:36 by mito              #+#    #+#             */
-/*   Updated: 2023/11/24 15:37:37 by mito             ###   ########.fr       */
+/*   Updated: 2023/11/27 12:22:38 by mito             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,19 +32,3 @@ int	print_ptr(unsigned long n)
 		return (-1);
 	return (count);
 }
-/*
-int	print_ptr(unsigned long n)
-{
-	int	count;
-
-	count = 0;
-	count += write (1, "0x", 2);
-	if (n == 0)
-	{
-		return (count + write (1, "0", 1));
-	}
-	return (count + print_ptraddress(n));
-	if (count == -1)
-		return (-1);
-}
-*/
