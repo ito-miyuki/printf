219a55d29596075904dd0d0bfa3f53978be38c4b
Miyuki Ito <mito@c1r3p7.hive.fi>
2024-05-06 11:31:01 +0300

    modified error handlings: return -1

--- ft_printf.c
+++ ft_printf.c
@@ -6,65 +6,35 @@
 /*   By: mito <mito@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 13:45:33 by mito              #+#    #+#             */
-/*   Updated: 2023/11/20 14:59:44 by mito             ###   ########.fr       */
+/*   Updated: 2023/11/28 14:10:12 by mito             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdarg.h>
-#include <unistd.h>
+#include "ft_printf.h"
 
-int print_char(int c)
+int	ft_printf(const char *str, ...)
 {
-    return (write(1, &c, 1));
+	va_list	ap;
+	int		count;
+
+	va_start(ap, str);
+	count = 0;
+	while (*str != '\0')
+	{
+		if (*str == '%')
+		{
+			count += print_format(*(++str), ap);
+			if (count < 0)
+				return (-1);
+		}
+		else
+		{
+			count += write(1, str, 1);
+			if (count < 0)
+				return (-1);
+		}
+		str++;
+	}
+	va_end(ap);
+	return (count);
 }
-
-int print_str(char *str)
-{
-    int i;
-
-    i = 0;
-    while (*str != '\0')
-    {
-        print_char((int)*str);
-        i++;
-        str++;
-    }
-    return (i);
-}
-int ft_print_format(char specifier, va_list ap)
-{
-    int count;
-
-    count = 0;
-    if (specifier == 'c')
-        print_char(va_arg(ap, int));
-    else if (specifier == 's')
-        print_str(va_arg(ap, char *));
-    else if (specifier == 'd')
-        print_digit((long)va_arg(ap, int), 10);
-    else if (specifier == 'x')
-        print_digit((long)va_arg(ap, unsigned int), 16);
-    else
-        count += write(1, &specifier, 1); //adrees of speifier
-    return (count);
-}
-
-int ft_printf(const char *str, ...)
-{
-    va_list ap;
-    int     count;
-
-    va_start(ap, str);
-    count = 0;
-    while (*str != '\0');
-    {
-        if (*str == '%')
-            count += ft_print_format(*(++str), ap);
-        else
-            count += write(1, str, 1);
-        str++;
-    }
-    va_end(ap);
-    return (count);
-}
-
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: mito <mito@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 10:43:02 by mito              #+#    #+#             */
-/*   Updated: 2023/11/24 12:16:59 by mito             ###   ########.fr       */
+/*   Updated: 2023/11/28 13:51:10 by mito             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,7 @@
 # define FT_PRINTF_H
 
 # include <unistd.h>
-# include <stdio.h>
 # include <stdarg.h>
-# include <limits.h>
 
 int	ft_printf(const char *str, ...);
 int	print_format(char specifier, va_list ap);
